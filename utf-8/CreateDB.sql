83cea6b630962b20bf07c9fe8c99ccbad7637a95
George Golikov <golikov.georgij@mail.ru>
2021-03-12 17:01:07 +0300

    utf-8

---
+++ utf-8/CreateDB.sql
@@ -0,0 +1,20 @@
+﻿USE Master
+
+Go
+CREATE DATABASE Session ON (
+	Name= Session, 
+	FileName='D:\Database\Session.mdf'
+) 
+LOG ON (
+	Name= Session_log, 
+	FileName='D:\Database\Session_log.ldf'
+) 
+Go
+
+EXEC SP_HELPDB Session
+
+ALTER DATABASE Session
+ MODIFY FILE (name=Session, maxsize=100MB)
+ Go
+ EXEC SP_HELPDB Session
+ Go
